cbcfd61970b6d5b9fdd8587dfdba1d74fb705e60
Nicu Gorea <nicug99@gmail.com>
2018-06-27 17:44:37 +0300

    Add project files.

---
+++ AdBagWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdBagWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
